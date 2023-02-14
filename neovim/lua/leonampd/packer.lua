7e34944db6f36f125efef289a102b11cddbe3bde
Leonam Pereira Dias <leonam.pd@gmail.com>
2023-02-14 20:05:47 -0300

    configuring neovim entirelly in lua

---
+++ neovim/lua/leonampd/packer.lua
@@ -0,0 +1,34 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use({
+		'rose-pine/neovim',
+		as = 'rose-pine',
+		config = function()
+			require("rose-pine").setup()
+			vim.cmd('colorscheme rose-pine')
+		end
+	})
+
+	use({"nvim-treesitter/nvim-treesitter", {run = ':TSUpdate'}})
+
+	use({"airblade/vim-gitgutter"})
+	use({"jiangmiao/auto-pairs"})
+	use({"APZelos/blamer.nvim"})
+	use({"ruanyl/vim-gh-line"})
+
+	use 'Olical/conjure'
+
+end)  
