262ad830fd91d458ec44e066efcc9a281311dbf8
Leonam Pereira Dias <leonam.pd@gmail.com>
2023-02-14 20:05:47 -0300

    configuring neovim entirelly in lua

---
+++ neovim/lua/leonampd/remap.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = " "
+vim.g.localleader = ","
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>w", vim.cmd.w)
+vim.keymap.set("n", "<leader>q", vim.cmd.q)
