e07e5e49fb56ba590a126a9f5a84172b8c807ccc
Leonam Pereira Dias <leonam.pd@gmail.com>
2023-02-15 11:05:16 -0300

    nvim: change to gruvbox and add more remaps

--- neovim/after/plugin/colors.lua
+++ neovim/after/plugin/colors.lua
@@ -1,6 +1,6 @@
 function Colorize(color)
-  color = color or "rose-pine",
-  vim.cmd.colorscheme(color)
+  vim.o.background = "dark" -- or "light" for light mode
+  vim.cmd([[colorscheme gruvbox]])
 
   vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
