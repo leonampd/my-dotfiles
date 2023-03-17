3a169c730fba00ad4c71838cc2df77ad3d55af5d
Leonam Pereira Dias <leonam.pd@gmail.com>
2023-03-17 18:18:29 -0300

    more nvim configs

--- neovim/lua/leonampd/packer.lua
+++ neovim/lua/leonampd/packer.lua
@@ -22,6 +22,35 @@ return require('packer').startup(function(use)
 	use({"APZelos/blamer.nvim"})
 	use({"ruanyl/vim-gh-line"})
 
-	use 'Olical/conjure'
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},         -- Required
+            {'hrsh7th/cmp-nvim-lsp'},     -- Required
+            {'hrsh7th/cmp-buffer'},       -- Optional
+            {'hrsh7th/cmp-path'},         -- Optional
+            {'saadparwaiz1/cmp_luasnip'}, -- Optional
+            {'hrsh7th/cmp-nvim-lua'},     -- Optional
+            {'PaterJason/cmp-conjure'},
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},             -- Required
+        }
+    }
+
+    use {
+        'Olical/conjure',
+        requires = {
+            {'hrsh7th/nvim-cmp'},
+            {'PaterJason/cmp-conjure'},
+            {'m00qek/baleia.nvim'}
+        }
+    }
 end)  
--- neovim/lua/leonampd/remap.lua
+++ neovim/lua/leonampd/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.g.localleader = ","
+vim.g.maplocalleader = ","
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>w", vim.cmd.w)
