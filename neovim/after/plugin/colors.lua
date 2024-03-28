1f763c5a43d578153226a9528a82bc16b0c002d1
Leonam Pereira Dias <leonam.pd@gmail.com>
2024-03-28 08:00:40 -0300

    organize dirs to use them as stow packages

--- neovim/after/plugin/colors.lua
+++
@@ -1,9 +0,0 @@
-function Colorize(color)
-  vim.o.background = "dark" -- or "light" for light mode
-  vim.cmd([[colorscheme gruvbox]])
-
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
-Colorize()
