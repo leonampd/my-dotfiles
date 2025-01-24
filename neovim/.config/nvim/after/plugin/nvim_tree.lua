d555e7da74c2a7398c70d4731d01cb6f4216f4cd
Leonam Dias <leonam.pd@gmail.com>
2025-01-24 07:41:57 -0300

    nvim: remove unnecessary line from nvim-tree config

--- neovim/.config/nvim/after/plugin/nvim_tree.lua
+++ neovim/.config/nvim/after/plugin/nvim_tree.lua
@@ -5,9 +5,6 @@ vim.g.loaded_netrwPlugin = 1
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
 
--- empty setup using defaults
-require("nvim-tree").setup()
-
 -- OR setup with some options
 require("nvim-tree").setup({
   sort = {
