ffa6c2c4cbc5b92da85bdeed60175e8a486f0b10
Leonam Dias <leonam.pd@gmail.com>
2025-12-04 19:24:42 -0300

    neovim: adjust some configs for coding clojure

--- neovim/dot-config/nvim/lua/config/lazy.lua
+++ neovim/dot-config/nvim/lua/config/lazy.lua
@@ -33,20 +33,20 @@ require("lazy").setup({
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
-    notify = false, -- notify on update
+    notify = true, -- notify on update
   }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
       disabled_plugins = {
-        "gzip",
+        -- "gzip",
         -- "matchit",
         -- "matchparen",
         -- "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
-        "zipPlugin",
+        -- "zipPlugin",
       },
     },
   },
